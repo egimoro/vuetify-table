f6c695c9be0f1f38173f727716b80e1b6e8df69c
egimoro <gimoremma@tutanota.com>
2021-12-10 00:02:43 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,98 @@
+<template>
+  <v-app id="inspire">
+    <v-navigation-drawer 
+      v-model="drawer"
+      app
+    >
+      <v-list-item>
+        <v-list-item-content>
+          <v-list-item-title class="text-h6">
+            Vuetify Todo
+          </v-list-item-title>
+          <v-list-item-subtitle>
+            Jaren's Vue App!
+          </v-list-item-subtitle>
+        </v-list-item-content>
+      </v-list-item>
+
+      <v-divider></v-divider>
+ 
+      <v-list
+        dense
+        nav
+      >
+        <v-list-item
+          v-for="item in items"
+          :key="item.title"
+          :to="item.to "
+          link
+        >
+          <v-list-item-icon>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-item-icon>
+
+          <v-list-item-content>
+            <v-list-item-title>{{ item.title }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-app-bar
+      app
+      color="primary"
+      dark
+      shrink-on-scroll
+      prominent 
+      src="michelle-kim.jpg"
+    >
+      <template v-slot:img="{ props }">
+        <v-img
+          v-bind="props"
+          gradient="to top right, rgba(100,115,201,.7), rgba(25,32,72,.7)"
+        ></v-img>
+      </template>
+
+      <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+
+      <v-app-bar-title
+        class="width-100"
+      >
+        Jaren's Todo list
+      </v-app-bar-title>
+      <p
+        class="width-100"
+      >
+        {{ currentDateTime() }}
+      </p>
+
+      <v-spacer></v-spacer> 
+    </v-app-bar>
+
+    <v-main>
+      <router-view></router-view>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+  export default {
+    data: () => ({  
+      drawer: null,
+      items: [
+          { title: 'Todo', icon: 'mdi-format-list-checks', to: '/' },
+          { title: 'About', icon: 'mdi-information-outline', to: '/about' },
+        ],
+    }),
+    methods: {
+      currentDateTime() {
+        const current = new Date();
+        const date =  (current.getMonth()+1) + '/' + current.getDate() + ', ' + current.getFullYear();
+        const time = current.getHours() + ":" + current.getMinutes();
+        const dateTime = date + ', ' + time;
+
+        return dateTime;
+      },
+    }
+  }
+</script>
